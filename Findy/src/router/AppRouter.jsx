5c497d221b534286759d05abcef813df65455caa
Gusnecio <gustavojosegarciaperez86@gmail.com>
2024-08-25 11:55:54 -0500

    correccion errores

--- Findy/src/router/AppRouter.jsx
+++ Findy/src/router/AppRouter.jsx
@@ -1,23 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
-<<<<<<< HEAD
 import Login from "../components/Login";
 import Register from "../components/Register"; 
-=======
->>>>>>> 74d764f250f8c994cbfae8445fef0ee166e4a065
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Layout />}>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} /> 
         </Route>
-=======
-        <Route path="/" element={<Layout />}></Route>
->>>>>>> 74d764f250f8c994cbfae8445fef0ee166e4a065
       </Routes>
     </Router>
   );
--- Findy/tailwind.config.js
+++ Findy/tailwind.config.js
@@ -1,14 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Añade esta línea
-  ],
-=======
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
->>>>>>> 74d764f250f8c994cbfae8445fef0ee166e4a065
   theme: {
     extend: {
       colors: {
@@ -20,8 +12,4 @@ export default {
     },
   },
   plugins: [],
-<<<<<<< HEAD
-}
-=======
 };
->>>>>>> 74d764f250f8c994cbfae8445fef0ee166e4a065
