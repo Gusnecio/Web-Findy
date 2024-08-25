4ffcbd937c07c6cfa5e4e6e291f3db277db1f237
Gusnecio <gustavojosegarciaperez86@gmail.com>
2024-08-25 15:38:47 -0500

    diseño del register, login y responsive

--- Findy/src/components/Login.jsx
+++ Findy/src/components/Login.jsx
@@ -1,121 +1,138 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"; // Importa useNavigate
+import { useNavigate } from "react-router-dom";
+import logoFindy from "/logo/LOGOFINDY.png";
 
-const Login = () => {
-  const [identifier, setidentifier] = useState("");
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
     setLoading(true);
-    setError("");
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      setLoading(false);
+      return;
+    }
 
     try {
       const response = await fetch(
         "https://fake-api-eight-gilt.vercel.app/users",
         {
-          method: "GET",
+          method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
+          body: JSON.stringify({ username, email, password }),
         }
       );
 
-      const data = await response.json();
-
       if (response.ok) {
-        const user = data.find(
-          (user) =>
-            (user.username === identifier || user.email === identifier) &&
-            user.password === password
-        );
-
-        if (user) {
-          console.log("Login successful:", user);
-          navigate("/layout");
-        } else {
-          setError("Invalid credentials");
-          console.error("No matching user found");
-        }
+        navigate("/login");
       } else {
-        setError("Failed to fetch users");
-        console.error("Error details:", data);
+        navigate("/");
       }
     } catch (error) {
-      setError("An error occurred. Please try again.");
-      console.error("Error:", error);
+      alert("An unexpected error occurred. Please try again later.", error);
     } finally {
       setLoading(false);
     }
   };
 
-  const handleRegisterRedirect = () => {
-    navigate("/register");
-  };
-
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold text-center">Login</h2>
-        <form className="space-y-4" onSubmit={handleLogin}>
-          <div>
-            <label
-              htmlFor="username o email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Username or Email
-            </label>
-            <input
-              id="email"
-              type="text"
-              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-              value={identifier}
-              onChange={(e) => setidentifier(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          {error && <p className="text-sm text-red-600">{error}</p>}
-          <button
-            type="submit"
-            className="w-full px-4 py-2 text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            disabled={loading}
-          >
-            {loading ? "Logging in..." : "Login"}
-          </button>
-        </form>
-        <div className="text-center">
-          <p className="text-sm text-gray-600">
-            ¿No tienes una cuenta?{" "}
+    <div className="min-h-screen bg-gradient-to-r from-[#FF74B7] via-[#FF7674] to-[#FF74FC] flex flex-col lg:flex-row">
+      <div className="w-full lg:w-1/2 flex items-center justify-center lg:justify-end lg:relative order-1 lg:order-2">
+        <img
+          className="w-full max-w-md h-auto object-cover"
+          src={logoFindy}
+          alt="Register background"
+        />
+      </div>
+      <div className="w-full lg:w-1/2 flex items-center justify-center p-6 lg:p-48 order-2 lg:order-1">
+        <div className="w-full max-w-md p-10 space-y-14 bg-white rounded-lg shadow-md">
+          <h2 className="text-2xl font-bold text-center">Register</h2>
+          <form className="space-y-4" onSubmit={handleRegister}>
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Usuario
+              </label>
+              <input
+                id="username"
+                type="text"
+                className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Correo Electrónico
+              </label>
+              <input
+                id="email"
+                type="email"
+                className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Contraseña
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="confirm-password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Confirmar Contraseña
+              </label>
+              <input
+                id="confirm-password"
+                type="password"
+                className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </div>
             <button
-              onClick={handleRegisterRedirect}
-              className="text-indigo-600 hover:text-indigo-700 font-medium"
+              type="submit"
+              className="w-full px-4 py-2 text-white bg-gradient-to-r from-[#FF74B7] via-[#FF7674] to-[#FF74FC] rounded-lg hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              disabled={loading}
             >
-              Regístrate
+              {loading ? "Registering..." : "Register"}
             </button>
-          </p>
+          </form>
         </div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
--- Findy/src/components/Register.jsx
+++ Findy/src/components/Register.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import logoFindy from "/logo/LOGOFINDY.png";
 
 const Register = () => {
   const [username, setUsername] = useState("");
@@ -9,7 +10,6 @@ const Register = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  // En tu componente Register
   const handleRegister = async (e) => {
     e.preventDefault();
     setLoading(true);
@@ -45,82 +45,91 @@ const Register = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold text-center">Register</h2>
-        <form className="space-y-4" onSubmit={handleRegister}>
-          <div>
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium text-gray-700"
+    <div className="min-h-screen bg-gradient-to-r from-[#FF74B7] via-[#FF7674] to-[#FF74FC] flex flex-col lg:flex-row">
+      <div className="w-full lg:w-1/2 flex items-center justify-center lg:justify-start lg:relative order-1 lg:order-2">
+        <img
+          className="w-full h-auto lg:w-full object-cover"
+          src={logoFindy}
+          alt="Register background"
+        />
+      </div>
+      <div className="w-full lg:w-1/2 flex items-center justify-center p-6 lg:p-48 order-2 lg:order-1">
+        <div className="w-full max-w-md p-10 space-y-14 bg-white rounded-lg shadow-md">
+          <h2 className="text-2xl font-bold text-center">Register</h2>
+          <form className="space-y-4" onSubmit={handleRegister}>
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Usuario
+              </label>
+              <input
+                id="username"
+                type="text"
+                className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Correo Electronico
+              </label>
+              <input
+                id="email"
+                type="email"
+                className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Contraseña
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="confirm-password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Confirmar Contraseña
+              </label>
+              <input
+                id="confirm-password"
+                type="password"
+                className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 text-white bg-gradient-to-r from-[#FF74B7] via-[#FF7674] to-[#FF74FC] rounded-lg hover:opacity-80 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              disabled={loading}
             >
-              Username
-            </label>
-            <input
-              id="username"
-              type="text"
-              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              id="email"
-              type="email"
-              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="confirm-password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Confirm Password
-            </label>
-            <input
-              id="confirm-password"
-              type="password"
-              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full px-4 py-2 text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            disabled={loading}
-          >
-            {loading ? "Registering..." : "Register"}
-          </button>
-        </form>
+              {loading ? "Registering..." : "Register"}
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
