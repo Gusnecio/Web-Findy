bc69fa3cf47cf8c89a84bd0bee87919daf04a9ab
Jhonier Santana <jhonier_2504@hotmail.com>
2024-08-28 10:43:41 -0500

    Responsive Main/Container

--- Findy/src/components/SideBar.jsx
+++ Findy/src/components/SideBar.jsx
@@ -5,7 +5,8 @@ import iconBuscar from "/icons/search.svg";
 import iconNoti from "/icons/heart.svg";
 import iconMensaje from "/icons/mensaje.svg";
 import iconCreate from "/icons/create.svg";
-import Modal from "./Modal"; // Asegúrate de importar el componente Modal
+import iconHistoria from "/histories/JennieKim.png";
+import Modal from "./Modal";
 
 const SideBar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -19,15 +20,15 @@ const SideBar = () => {
   };
 
   return (
-    <nav className="lg:flex fixed z-10 lg:w-[250px] w-full lg:top-0 lg:left-0 bottom-0 lg:h-[100vh] lg:ml-3 lg:items-center lg:justify-center border-r-2 border-gray-300 lg:bg-white bg-pastel lg:rounded-b-none rounded-b-[25%]">
+    <nav className="lg:flex fixed z-10 lg:w-[250px] lg:p-0 p-2 w-full lg:top-0 lg:left-0 bottom-0 lg:h-[100vh] lg:ml-3 lg:items-center lg:justify-center border-r-2 border-gray-300 lg:bg-white bg-nav-bg bg-cover bg-no-repeat bg-bottom lg:rounded-b-none rounded-b-[30px] lg:bg-none">
       <div className="lg:flex lg:flex-col">
         <div className="mb-10 lg:flex hidden">
           <img src={logoFindy} alt="LogoFIndy" />
         </div>
-        <ul className="flex lg:flex-col lg:w-[38%] lg:space-y-11 font-balsamiq lg:text-[20px] justify-around lg:bg-none ">
+        <ul className="flex lg:flex-col lg:w-[38%] lg:space-y-11 font-balsamiq lg:text-[20px] justify-between lg:bg-none ">
           <li className="flex cursor-pointer hover:text-pastel items-center">
             <img
-              className="mr-3 lg:invert-0 invert"
+              className="lg:mr-3 lg:invert-0 invert"
               src={iconInicio}
               alt="iconInicio"
             />
@@ -35,41 +36,39 @@ const SideBar = () => {
           </li>
           <li className="flex cursor-pointer hover:text-pastel items-center">
             <img
-              className="mr-3 lg:invert-0 invert"
+              className="lg:mr-3 lg:invert-0 invert"
               src={iconBuscar}
               alt="iconBuscar"
             />
             <span className="lg:flex hidden">Busqueda</span>
           </li>
           <li
-            className="flex cursor-pointer hover:text-pastel items-center"
+            className="flex cursor-pointer hover:text-pastel items-center justify-center"
             onClick={handleCreateClick}
           >
-            <img
-              className="mr-3 lg:mb-0 mb-10 lg:invert-0 invert"
-              src={iconCreate}
-              alt="iconCreate"
-            />
-            <span className="lg:flex hidden">Crear</span>
+            <div className="lg:flex lg:relative absolute lg:top-0 top-[-30px] lg:bg-transparent bg-pastel lg:rounded-none rounded-full flex z-10 lg:w-[85px] lg:h-[24px] w-[65px] h-[65px] lg:justify-normal justify-center lg:left-0 left-[42%]">
+              <img
+                className="lg:mr-3 lg:invert-0 invert lg:w-[24px] w-10"
+                src={iconCreate}
+                alt="iconCreate"
+              />
+              <span className="lg:flex hidden ">Crear</span>
+            </div>
           </li>
           <li className="flex cursor-pointer hover:text-pastel items-center">
             <img
-              className="mr-3 lg:invert-0 invert"
+              className="lg:mr-3 lg:invert-0 invert"
               src={iconNoti}
               alt="iconNotificaciones"
             />
             <span className="lg:flex hidden">Notificaciones</span>
           </li>
           <li className="lg:flex hidden cursor-pointer hover:text-pastel items-center">
-            <img className="mr-3" src={iconMensaje} alt="iconMensaje" />
+            <img className="lg:mr-3" src={iconMensaje} alt="iconMensaje" />
             Mensajes
           </li>
           <li className="flex cursor-pointer hover:text-pastel items-center">
-            <img
-              className="mr-3 lg:invert-0 invert"
-              src={iconNoti}
-              alt="iconPerfil"
-            />
+            <img className="lg:mr-3 w-10" src={iconHistoria} alt="iconPerfil" />
             <span className="lg:flex hidden">Perfil</span>
           </li>
         </ul>
